007322aac59700b9109d1bc91a9445d1af7e188b
Maxim Bernatsky <maxsateon@yandex.ru>
2023-04-03 23:26:50 +0300

    work on mistake

--- backend/app.js
+++ backend/app.js
@@ -6,9 +6,8 @@ const router = require('./routes/index');
 const { middlewaresError } = require('./middlewares/middlewaresError');
 
 const { PORT = 3000 } = process.env;
-const app = express();
-app.use(cors());
 mongoose.connect('mongodb://127.0.0.1/mestodb');
+const app = express();
 
 app.use(express.json());
 
